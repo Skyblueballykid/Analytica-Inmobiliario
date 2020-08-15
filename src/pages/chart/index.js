a6121e72f97f2e002ae11e205ef298a235a791c8
Thomas Kalnik <kalnik.thomas@gmail.com>
2020-08-14 21:27:53 -0500

    charting

--- src/pages/chart/index.js
+++ src/pages/chart/index.js
@@ -2,10 +2,12 @@ import { Chart1 } from './Chart1';
 import { Chart2 } from './Chart2';
 import { Chart3 } from './Chart3';
 import { Chart4 } from './Chart4';
+import { Chart5 } from './Chart5';
 
 export {
     Chart1,
     Chart2,
     Chart3,
-    Chart4
+    Chart4,
+    Chart5
 }
